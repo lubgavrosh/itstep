70746c346f52fbb1476ee6d801ff1ffb20939185
lubgavrosh <90957221+lubgavrosh@users.noreply.github.com>
2021-12-06 19:44:48 +0200

    Add files via upload
    evelop a String class that will then be used to work with strings. The class must contain:
    dynamic field type char * to save the string,
    methods
    • default constructor, which allows you to create a string of 80 characters, the contents of the string "" (empty string);
    • constructor that allows you to create a string of any size, the contents of the string "" (empty string);
    • a constructor that creates a string and initializes it with a string received from the user;
    • copy designer;
    • methods for entering lines from the keyboard and outputting lines to the screen (note that the line you enter may have arger size) 
    • an instance method that returns a new string (String) formed by concatenating (combining) two strings (for example, combining strings “Program” and “C ++” will be “Program C ++” (indicative prototype of the method - String concat (const String & obj))
    • an instance method that returns a new string (String) formed by the intersection (common characters) of two strings (for example, the intersection of the strings “programming” and “language” will be “gan” 
    • Assignment operator.

---
+++ 29.11.2021 class String home.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <string>
+#include<cstring>
+#include<Windows.h>
+using namespace std;
+
+class String
+{
+private:
+    char* ch;
+    char* s;
+ 
+    string d;
+    string j;
+    string e=" ";
+    string a;
+    const int SIZE = 80;
+public:
+    String() 
+    {
+       
+        ch= new char[SIZE];
+        s = new char [SIZE];
+     
+      
+    }
+    String(char* ch,char*s)
+    {
+       ch = new char;
+       s = new char;
+    }
+
+    String(const String& ch, const String &s)
+    {}
+    
+    void Initialization()
+    {
+       
+       
+        cout << "Enter first string: ";
+        cin.getline(ch, 80);
+        cout<< endl;
+        cout << "Enter second string: ";
+        cin.getline(s, 80);
+       
+    }
+    void Show() const
+    {
+        cout << "String1: " << ch<< endl;
+        cout << "String2: " << s << endl;
+    }
+    void association()
+    {
+       
+        cout << "Association String(1&2): ";
+         strcat_s(ch, 80,s);
+        cout << ch << endl;
+        Sleep(2000);
+        
+     }
+    void cross()
+    {
+        system("cls");
+        cout << "Enter  first string:";
+        cin >> d;
+        cout << endl;
+        cout << "Enter second string: ";
+        cin >> j;
+        int count=0;
+        char a1, a2='0';
+      
+        for ( int i = 0; i <= j.length();i++)
+        {
+            for (int x = 0; x < d.length(); x++)
+            {
+                if (j[i] == d[x])
+                {
+                    
+                        a1 = j[i];
+                        if (a1 != a2 )
+                        {
+                            a2 = a1;
+                         
+                                    e.push_back(a1);
+                                
+                           
+                         
+                        }
+                    
+                }
+            }
+
+        }
+        int n,m;
+        for ( n=0; n < e.size(); n++)
+        {
+            if (e[n] == e[n + 1])
+            {
+                e.erase(n, 1);
+            }
+        }
+        a = e;
+        for (n = 0; n < e.size(); n++)
+        {
+            for (m=n+1 ; m < a.size()+1; m++)
+            {
+                if (e[n] == a[m])
+                {
+                    a.erase(m, 1);
+                }
+            }
+        }
+        cout << "The same letter of String 1 & 2: "<<a;
+ }
+    
+    ~String()
+    {
+        delete[] ch;
+        delete[] s;
+       
+    }
+};
+
+void main()
+{
+    String s;
+    s.Initialization();
+    s.Show();
+    s.association();
+    s.cross();
+    
+
+
+
+    
+
+}
+
+
+
+
